9fb51cbbfc202d6e5b82b6aa38104bc3752e3620
vuong <oscar.nguyen.goldenowl@gmail.com>
2019-08-15 17:21:45 +0700

    separate routes become RouteComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Fragment, Component } from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import Home from './containers/Home';
-import Navigation from './containers/Navigation';
-import ProductCategory from './containers/ProductCategory';
-import Loading from './components/loading';
-import Signin from './components/signin';
-import Signup from './components/signup';
+import {BrowserRouter as Router} from "react-router-dom";
+import Routes from './containers/Routes';
 import ErrorBoundary from './components/errorBoundary';
 
 import "./App.css";
@@ -16,13 +11,8 @@ class App extends Component {
     return (
       <Fragment>
         <Router>
-          <Navigation/>
-          <Loading/>
           <ErrorBoundary>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/products/:slug" component={ProductCategory}/>
-            <Route exact path="/signin" component={Signin}/>
-            <Route exact path="/signup" component={Signup}/>
+            <Routes/>
           </ErrorBoundary>
         </Router>
       </Fragment>
