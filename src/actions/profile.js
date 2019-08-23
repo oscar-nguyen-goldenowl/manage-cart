3d56d17f8fb3770286f7b791637f0771a0849b06
vuong <oscar.nguyen.goldenowl@gmail.com>
2019-08-23 08:52:42 +0700

    custom API update profile

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,5 +1,8 @@
 export const GET_PROFILE_SUCCESS = "GET_PROFILE_SUCCESS";
 export const GET_PROFILE_ERROR = "GET_PROFILE_ERROR";
+export const CHANGE_PROFILE = "CHANGE_PROFILE";
+
+
 
 export const getProfileSuccess = (profile) => {
   return {
@@ -12,5 +15,12 @@ export const getProfileError = (error) => {
   return {
     type: GET_PROFILE_ERROR,
     error
+  }
+}
+
+export const changeProfile = (profile) => {
+  return {
+    type: CHANGE_PROFILE,
+    profile
   }
 }
